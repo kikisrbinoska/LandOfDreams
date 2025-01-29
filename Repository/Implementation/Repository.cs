655d5961ab1b9c3f9e85e6f160c2b0e0283f9110
kikisrbinoska <srbinoskakristina07@gmail.com>
2025-01-29 23:52:51 +0100

    Added repository

---
+++ Repository/Implementation/Repository.cs
@@ -0,0 +1,99 @@
+﻿using Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using Repository.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository.Implementation
+{
+    public class Repository<T> : IRepository<T> where T : BaseEntity
+    {
+        private readonly ApplicationDbContext context;
+        private DbSet<T> entities;
+       
+
+        public Repository(ApplicationDbContext context)
+        {
+            this.context = context;
+            entities = context.Set<T>();
+        }
+        public IEnumerable<T> GetAll()
+        {
+            if (typeof(T).IsAssignableFrom(typeof(Booking)))
+            {
+                return entities
+                    .Include("Apartment")
+                    .Include("Hotel")
+                    .Include("Destionation")
+                    .AsEnumerable();
+            }
+            else
+            {
+                return entities.AsEnumerable();
+            }
+        }
+
+        public T Get(int? id)
+        {
+            if (typeof(T).IsAssignableFrom(typeof(Booking)))
+            {
+                return entities
+                    .Include("Apartment")
+                    .Include("Hotel")
+                    .Include("Destionation")
+                    .First(s => s.Id == id);
+            }
+            else
+            {
+                return entities.First(s => s.Id == id);
+            }
+
+        }
+        public T Insert(T entity)
+        {
+            if (entity == null)
+            {
+                throw new ArgumentNullException("entity");
+            }
+            entities.Add(entity);
+            context.SaveChanges();
+            return entity;
+        }
+
+        public T Update(T entity)
+        {
+            if (entity == null)
+            {
+                throw new ArgumentNullException("entity");
+            }
+            entities.Update(entity);
+            context.SaveChanges();
+            return entity;
+        }
+
+        public T Delete(T entity)
+        {
+            if (entity == null)
+            {
+                throw new ArgumentNullException("entity");
+            }
+            entities.Remove(entity);
+            context.SaveChanges();
+            return entity;
+        }
+
+        public List<T> InsertMany(List<T> entities)
+        {
+            if (entities == null)
+            {
+                throw new ArgumentNullException("entities");
+            }
+            entities.AddRange(entities);
+            context.SaveChanges();
+            return entities;
+        }
+    }
+}
